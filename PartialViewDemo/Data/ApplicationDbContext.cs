bdad247ea70b518abe698cb6c9f52d26d095c810
Suraj Fulsoundar <100521841+SurajFulsoundar@users.noreply.github.com>
2024-05-01 16:34:42 +0530

    Add project files.

---
+++ PartialViewDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartialViewDemo.Models;
+
+namespace PartialViewDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ PartialViewDemo/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartialViewDemo.Models
+{
+    [Table("Book1")]
+    public class Book
+    {
+       public int Id { get; set; }
+       public string? Name { get; set; }
+       public string? Author { get; set; }
+       public int Price { get; set; }
+        
+    }
+}
